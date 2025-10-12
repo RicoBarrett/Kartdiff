2f33c55c637fc3f58aa552821ebce10cc2c28b02
V <146790727+vilcodes@users.noreply.github.com>
2025-10-12 16:35:33 +0100

    update to powerups + new minimap
    small update to powerups.
    jail delay for use is fixed
    speed not yet
    train still requires testing and better pathing
    
    new minimap added

--- Kartiff/Assets/VVPWR/PWR_Jail.cs
+++ Kartiff/Assets/VVPWR/PWR_Jail.cs
@@ -1,28 +1,35 @@
 using UnityEngine;
+using System.Collections;
 
 public class PWR_Jail : MonoBehaviour
 {
+    [Header("powerup settings")]
     public float respawnTime = 5f; // how long till powerup comes back
     public float stunDuration = 3f; // how long dummy is frozen
     public KeyCode useKey = KeyCode.E; // key to activate
 
+    // internal variables
     private bool isCollected = false; // if someone picked it up
+    private bool canUse = false; // if we can use it right now
     private bool isUsing = false; // if it's currently being used
     private PlayerMovementBarebones targetMovement; // the dummy we stun
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        // only trigger if a player touches it
+        // only trigger if player touches it
         if (other.CompareTag("Player") && !isCollected)
         {
             isCollected = true;
+            canUse = true; // allow using instantly
             Debug.Log("jail powerup picked up");
 
-            // hide powerup while it respawns
-            gameObject.SetActive(false);
+            // hide visuals but keep script alive
+            HidePickupVisuals();
+
+            // start timer for respawn
             Invoke(nameof(RespawnPowerUp), respawnTime);
 
-            // find the dummy in the scene to stun
+            // find dummy to target (the one that gets stunned)
             GameObject dummy = GameObject.FindWithTag("Dummy");
             if (dummy != null)
             {
@@ -35,10 +42,10 @@ public class PWR_Jail : MonoBehaviour
         }
     }
 
-    void Update()
+    private void Update()
     {
-        // press E to use it
-        if (isCollected && !isUsing && Input.GetKeyDown(useKey))
+        // check if player can use the powerup
+        if (canUse && !isUsing && Input.GetKeyDown(useKey))
         {
             if (targetMovement != null)
             {
@@ -52,29 +59,63 @@ public class PWR_Jail : MonoBehaviour
         }
     }
 
-    private System.Collections.IEnumerator UseJail()
+    private IEnumerator UseJail()
     {
         isUsing = true;
+        canUse = false;
 
-        // store dummy's speed so we can reset it
+        // store dummy speed to restore later
         float originalSpeed = targetMovement.moveSpeed;
-        targetMovement.moveSpeed = 0f; // stun the dummy
 
+        // freeze the dummy
+        targetMovement.moveSpeed = 0f;
+        Debug.Log("dummy stunned for " + stunDuration + " seconds");
+
+        // wait for duration
         yield return new WaitForSeconds(stunDuration);
 
-        targetMovement.moveSpeed = originalSpeed; // back to normal
+        // unfreeze dummy
+        targetMovement.moveSpeed = originalSpeed;
         isUsing = false;
         isCollected = false;
         Debug.Log("jail powerup ended");
     }
 
-    void RespawnPowerUp()
+    private void RespawnPowerUp()
     {
-        // bring the powerup back
-        gameObject.SetActive(true);
+        // bring powerup visuals back
+        ShowPickupVisuals();
+        Debug.Log("jail powerup respawned");
+    }
+
+    private void HidePickupVisuals()
+    {
+        // turns off all meshes and sprites so it's invisible
+        var renderers = GetComponentsInChildren<MeshRenderer>();
+        foreach (var r in renderers) r.enabled = false;
+
+        var sprites = GetComponentsInChildren<SpriteRenderer>();
+        foreach (var s in sprites) s.enabled = false;
+
+        // disable collider so player can't grab it again yet
+        var col = GetComponent<Collider>();
+        if (col != null) col.enabled = false;
+    }
+
+    private void ShowPickupVisuals()
+    {
+        // turns on all meshes and sprites again
+        var renderers = GetComponentsInChildren<MeshRenderer>();
+        foreach (var r in renderers) r.enabled = true;
+
+        var sprites = GetComponentsInChildren<SpriteRenderer>();
+        foreach (var s in sprites) s.enabled = true;
+
+        // re-enable collider so it can be picked up again
+        var col = GetComponent<Collider>();
+        if (col != null) col.enabled = true;
     }
 }
-
 
 //script was made using unity built in trigger and coroutine systems
 //https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnTriggerEnter.html
