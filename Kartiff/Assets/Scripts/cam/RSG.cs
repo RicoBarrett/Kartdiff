b5be135434c649b7b35194fa44384fc445011470
MrRodriguez96 <st20234956@outlook.cardiffmet.ac.uk>
2025-10-13 14:44:58 +0100

    made a simple 3210 for start game
    simply pauses game timescale and disables pause script when hit 0

--- Kartiff/Assets/Scripts/cam/RSG.cs
+++ Kartiff/Assets/Scripts/cam/RSG.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RSG : MonoBehaviour
 {
     public GameObject pauseMenu;
+    private float startingTime = 5f;
+    float currentTime;
+
+    [SerializeField] Text countdownText;
 
     void Start()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        currentTime = startingTime;
+
+        Debug.Log("Timer starts at: " + currentTime);
+    }
+
+    void Update()
+    {
+        currentTime -= Time.unscaledDeltaTime; // Changed because i forgot what "pausing" does
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
     }
 }
